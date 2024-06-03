9d9a8c87565cb51df9bc7aaaa4e70fe7e3a378a5
Johnz555 <7167166@MAIL.RU>
2024-06-03 21:45:03 +0300

    Add files via upload

---
+++ Базовые структуры данных.py
@@ -0,0 +1,42 @@
+print(5)
+print(type(5))
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5.5)
+print(5//4.5)
+print(5**5)
+print(type(22.5))
+print('Hello, World!')
+print('Hello, '+'World!')
+print(type(float('5')))
+print(len('Hello, World!'))
+print(6,5)
+print(6+5)
+print(6-5)
+# word length
+a = 'Hello, Wold!'
+print(len(a))
+# sums and differences
+first = 89
+second = 134
+summa = first+second
+diff = first-second
+print(summa)
+print(diff)
+# arithmetic mean
+first = 23
+second = 34
+third = 45
+mean = (first+second+third)//3
+print(mean)
+# simple strings
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string+", "+first_string)
+# A complex formula
+a = 35
+b = 47
+c = 15
+f = ((a*b)+(a*c))**3/2
+print(f)
